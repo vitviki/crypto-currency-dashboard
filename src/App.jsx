e68f9a040acb47e24e4f9e8f5ae8c77b38d0235f
vitviki <vtyagi1991@gmail.com>
2024-05-20 16:27:29 +0530

    output log

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
 
 export default function App() {
+  console.log(import.meta.env.VITE_REACT_APP_COINGEKO_BASE_URL);
   return (
     <main className="h-screen w-screen">
       <Header />
